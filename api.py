5ac5cf65577a0bf6c8ee03bc5d25ec4a7ad316bb
trevorhudson <trevor.c.hudson@gmail.com>
2022-12-06 21:17:28 -0800

    Add file download method to AWS_api class

--- api.py
+++ api.py
@@ -1,31 +1,68 @@
-from app import s3
 from werkzeug.utils import secure_filename
-
-import boto3
-from boto3.s3.transfer import TransferConfig
 from botocore.exceptions import ClientError
+from app import s3_resource, s3_client
 
 
-class AWS_Api:
+class AWS_api:
     ''' Interact with AWS API '''
 
+    # Fetch file from AWS
+
     @classmethod
-    def upload(self, file, bucket: str, key: str) -> bool:
+    def download(self, bucket_name, file_name, object_name=None):
+        '''
+        Download a file from S3.
+
+            Params:
+                bucket: target s3 bucket
+                file_name: OUTPUT file_name
+                object_name: name as stored on s3
+
+            '''
+
+        if object_name is None:
+            object_name = file_name
+
+        try:
+            with open(file_name, 'wb') as file:
+                file = s3_client.download_fileobj(
+                    bucket_name,
+                    object_name,
+                    file
+                )
+                return ('file', file)
+        except ClientError as error:
+            print(error)
+            return False
+
+    @classmethod
+    def upload(self, file_name, bucket_name, object_name=None, metadata={}) -> bool:
         '''
         Uploads a file to S3 bucket.
 
             Params:
-                file: File to upload
+                file_name: File to UPLOAD (binary)
                 bucket: target s3 Bucket
-                key: identifier, uses filename if not supplied.
+                object_name: s3 name -> defaults to file_name if not supplied.
+                metadata: optional key: value pair
             Returns:
                 True if successful upload, else False
         '''
 
+        if object_name is None:
+            object_name = file_name
+
         try:
-            with open(file, 'rb') as f:
-                s3.upload_fileobj(f, bucket, key)
+            with open(file_name, 'rb') as file:
+                s3_client.upload_fileobj(
+                    file,
+                    bucket_name,
+                    object_name,
+                    ExtraArgs={
+                        'Metadata': metadata})
         except ClientError as error:
             print(error)
             return False
-        return True
+        return file_name
+
+    # Update
