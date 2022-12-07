8e48637e5e2a714421bfe2635f9404cdbf47fbdc
trevorhudson <trevor.c.hudson@gmail.com>
2022-12-06 22:48:13 -0800

    Added methods to parse file URL:     signed_url() => generates signed, timed public url.     object_url() => generates direct URL to object. access is         AWS permission dependent.

--- api.py
+++ api.py
@@ -9,34 +9,48 @@ class api:
     # Fetch file from AWS
 
     @classmethod
-    def createURL(self, bucket_name, key):
+    def signed_url(self, bucket_name, key, expires=3600):
+        '''
+        Fetch signed URL from AWS S3.
+            Params:
+                bucket_name: target s3 bucket
+                key: name as stored on s3
+                expires: in seconds
+            Returns: string
+
+        '''
 
         url = s3_client.generate_presigned_url(
             ClientMethod='get_object',
             Params={'Bucket': bucket_name, 'Key': key},
-            ExpiresIn=3600)
+            ExpiresIn=expires)
         return url
 
     @classmethod
-    def download(self, bucket_name, file_name, object_name=None):
+    def object_url(self, bucket_name, key):
+        ''' Construct Object URL -> requires read access '''
+        return f'https://{bucket_name}.s3.us-west-1.amazonaws.com/{key}'
+
+    @classmethod
+    def download(self, bucket_name, file_name, key=None):
         '''
         Download a file from S3.
 
             Params:
-                bucket: target s3 bucket
+                bucket_name: target s3 bucket
                 file_name: OUTPUT file_name
-                object_name: name as stored on s3
+                key: name as stored on s3
 
             '''
 
-        if object_name is None:
-            object_name = file_name
+        if key is None:
+            key = file_name
 
         try:
             with open(file_name, 'wb') as file:
                 file = s3_client.download_fileobj(
                     bucket_name,
-                    object_name,
+                    key,
                     file
                 )
                 return ('file', file)
@@ -45,28 +59,28 @@ class api:
             return False
 
     @classmethod
-    def upload(self, file_name, bucket_name, object_name=None, metadata={}) -> bool:
+    def upload(self, file_name, bucket_name, key=None, metadata={}) -> bool:
         '''
         Uploads a file to S3 bucket.
 
             Params:
                 file_name: File to UPLOAD (binary)
                 bucket: target s3 Bucket
-                object_name: s3 name -> defaults to file_name if not supplied.
+                key: s3 name -> defaults to file_name if not supplied.
                 metadata: optional key: value pair
             Returns:
                 True if successful upload, else False
         '''
 
-        if object_name is None:
-            object_name = file_name
+        if key is None:
+            key = file_name
 
         try:
             with open(file_name, 'rb') as file:
                 s3_client.upload_fileobj(
                     file,
                     bucket_name,
-                    object_name,
+                    key,
                     ExtraArgs={
                         'Metadata': metadata})
         except ClientError as error:
