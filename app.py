5f4581c6c0401232c75cd56c3afc4608e7186f43
trevorhudson <trevor.c.hudson@gmail.com>
2022-12-07 21:01:09 -0800

    Added consistent logging for new requests

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request
 from flask_cors import CORS
-from AWS import AWS
+from AWS.AWS import AWS
 
 app = Flask(__name__)
 CORS(app)
@@ -38,10 +38,20 @@ app.config['S3_SECRET'] = os.environ["AWS_ACCESS_SECRET"]
 
 BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
 
+############################# Before All Requests ################################
+
+
+@app.before_request
+def log_request():
+    """Confirms receipt of request, and anything passed to it. """
+
+    print(
+        f' -----> BACKEND API - INCOMING REQUEST -----> {request}')
+
 
 ############################# Image Upload ################################
 
-@app.route('/upload', methods=["POST"])
+@ app.route('/upload', methods=["POST"])
 def upload_image():
     """ Handle image upload. Adds image and returns data about new image.
     """
@@ -51,18 +61,21 @@ def upload_image():
     description = request.form.get('description') or None
 
     print(
-        f' -----> BACKEND API - POST /upload received: file: {file} title: {title} description: {description}')
+        f' -----> BACKEND API - POST /upload -----> file: {file} title: {title} description: {description}')
 
     if not file:
         return {'error': 'Provide a valid image'}
 
     try:
         image = Image.create(file, BUCKET_NAME, title, description)
-        print('Successfully created image', image)
+
     except ValueError as e:
         print(e)
 
     db.session.commit()
+
+    print(
+        f' -----> BACKEND API - OUTGOING RESPONSE ----->  image created: {image}')
 
     return {"url": image.image_url,
             'file_name': image.file_name,
@@ -71,12 +84,37 @@ def upload_image():
             'creation_date': image.creation_date}
 
 
+############################# Image Download ################################
+
+@ app.route('/images/<file_name>', methods=['GET'])
+def get_image_data(file_name):
+    ''' Returns image data from backend. '''
+
+    if not file_name:
+        return {'error': 'Invalid file name'}
+
+    print(
+        f' -----> BACKEND API - GET /:file_name -----> file_name: {file_name}')
+    try:
+        image = Image.get_image(file_name, BUCKET_NAME)
+        print(
+            f' -----> BACKEND API - GET /:file_name ----->  image: {image}')
+    except ValueError as e:
+        print(e)
+
+    return {"url": image.image_url,
+            'file_name': image.file_name,
+            'title': image.title,
+            'description': image.description,
+            'creation_date': image.creation_date}
+
 ##############################################################################
 # Turn off all caching in Flask
 #   (useful for dev; in production, this kind of stuff is typically
 #   handled elsewhere)
 #
 # https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+
 
 @ app.after_request
 def add_header(response):
